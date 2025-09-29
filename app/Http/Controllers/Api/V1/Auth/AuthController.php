6bbdaa7779e7b4a4c80be8886d6eb528976579a5
Gabriel Matheus Silva <gabrielmath@hotmail.com>
2025-09-29 00:53:03 -0300

    #3 feat(auth): implemented patient authentication

---
+++ app/Http/Controllers/Api/V1/Auth/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\V1\AuthRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response as HTTPResponse;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function login(AuthRequest $request): JsonResponse
+    {
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            return response()->json([
+                'status'  => false,
+                'message' => 'Email & Password does not match with our record.',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = User::whereEmail($request->validated('email'))->first();
+
+        return response()->json([
+            'status'  => true,
+            'message' => 'User logged in successfully',
+            'token'   => $user->createToken('API REST TOKEN')->plainTextToken
+        ], Response::HTTP_OK);
+    }
+
+    public function logout(): HTTPResponse
+    {
+        if (Auth::check()) {
+            Auth::user()->tokens()->delete();
+        }
+
+        return response()->noContent();
+    }
+}

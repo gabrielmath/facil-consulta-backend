6642757a4c31b78cf78a9568dd37a41fcb995559
Gabriel Matheus Silva <gabrielmath@hotmail.com>
2025-09-29 00:55:30 -0300

    #3 feat(auth): implemented route of patient authentication

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Auth\AuthController;
 use App\Http\Controllers\Api\V1\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', [HomeController::class, 'test'])->name('test');
 
-/*Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'login'])->name('login');
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('logout', [AuthController::class, 'logout'])->name('logout');
-});*/
+});
